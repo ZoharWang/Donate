c79ffd4c41b94a61de816a6f506d229d821cf53f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-08 16:40:49 +0000

    Deploy EdisonJwa/Donate to github.com/EdisonJwa/Donate.git:gh-pages

---
+++ precache-manifest.b01477f93c8d8ba1496edc7ada85b8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137c87694ab96d7c86b08c72fded6f64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73d547e9a6398b744de4",
+    "url": "/static/css/2.848db5fd.chunk.css"
+  },
+  {
+    "revision": "431fa39a8a8963198aa2",
+    "url": "/static/css/main.78b3a575.chunk.css"
+  },
+  {
+    "revision": "73d547e9a6398b744de4",
+    "url": "/static/js/2.d97c7c23.chunk.js"
+  },
+  {
+    "revision": "431fa39a8a8963198aa2",
+    "url": "/static/js/main.481ceb49.chunk.js"
+  },
+  {
+    "revision": "02ef9e89453c0d205adf",
+    "url": "/static/js/runtime~main.ca3ec98b.js"
+  }
+]);
